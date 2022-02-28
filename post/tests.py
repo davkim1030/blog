66881fa9b7a31ea00e83c1e18651b54d065dd46f
davkim1030 <davkim1030@gmail.com>
2022-02-28 19:05:41 +0900

    add testcases

---
+++ post/tests.py
@@ -0,0 +1,35 @@
+import requests
+
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.test import TestCase
+from django.urls import reverse
+
+from post import views
+from post.models import Post
+
+
+class TestPost(TestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username='test_user_1', email='test_user_1', password='password1')
+        self.post = Post.objects.create(title='post test 1', author=self.user, content='This is content of tes post 1')
+
+    def tearDown(self) -> None:
+        User.objects.filter(id=self.user.id).delete()
+
+    def test_get_post_detail(self):
+        # When: calling existing post id
+        actual_response = self.client.get(reverse(views.detail, kwargs={'post_id': self.post.id}))
+
+        # Then: returns proper response
+        post = Post.objects.get(id=self.post.id)
+        values = post.__dict__
+        values['author'] = post.author.username
+        self.assertEqual(actual_response.content, render(None, 'post_detail.html', values).content)
+
+    def test_get_post_detail_not_existing(self):
+        # When: calling not existing post id
+        actual_response = self.client.get(reverse(views.detail, kwargs={'post_id': 0}))
+
+        # Then: gets 404 not found
+        self.assertEqual(actual_response.status_code, 404)
